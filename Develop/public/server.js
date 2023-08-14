b329581199d0dc5e54e487e49fcb541b0780062f
Mr. E <mr.e@Katrinas-iMac.local>
2023-08-14 18:41:52 -0400

    fixed errors

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -26,11 +26,11 @@ app.post('/api/notes', (req, res) => {
 
 // HTML routes
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+  res.sendFile(path.join(__dirname, 'notes.html'));
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Start the server
